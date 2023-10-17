4c8ca3d8f8c897b4c9752471841341a27a61b54e
[your <matheuscastroks@gmail.com>
2023-10-17 03:14:27 -0300

    criacao da classe pessoa e definicao dos metodos da classe

--- Pessoa.php
+++ Pessoa.php
@@ -1,57 +1,54 @@
 <?php
+class Pessoa {
+    private $pdo;
+    public function __construct($dbName, $host, $user, $password) {
 
-$rows = array();
-
-// Conexao com o banco
-
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=crudpdo', 'root');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $query = "SELECT id, nome, telefone, email FROM pessoa";
-    $stmt =  $pdo->query($query);
-    $rows =  $stmt->fetchAll(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    echo "<b>Erro com o banco de dados: </b>" . $e->getMessage();
-} catch (Exception $e) {
-    echo "<b>Erro generico: </b>" . $e->getMessage();
-}
-
-// DELETE
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['Excluir'])) {
-        $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
-        if ($id !== false) {
-            $delete = $pdo->prepare("DELETE FROM pessoa WHERE id = :id");
-            $delete->bindValue(":id", $id);
-            $delete->execute();
-            if ($delete) {
-                echo 'Usuário deletado com sucesso.';
-            } else {
-                echo 'Erro ao deletar o usuário.';
-            }
-        }
-    } else {
-        // Se o botão "Excluir" não foi clicado, então estamos tratando de um envio de dados do formulário
-
-        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-        $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_STRING);
-        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-
-        $insert = $pdo->prepare("INSERT INTO pessoa (nome, telefone, email) VALUES (:nome, :telefone, :email)");
-
-        $insert->bindValue(":nome", $nome);
-        $insert->bindValue(":telefone", $telefone);
-        $insert->bindValue(":email", $email);
-
-        $insert->execute();
-
-        if ($insert) {
-            echo 'Dados inseridos com sucesso.';
-        } else {
-            echo 'Erro ao inserir os dados.';
+        try {
+            $this->pdo = new PDO("mysql:dbname=" . $dbName . ";host=" . $host, $user, $password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            throw new Exception("Erro com o banco de dados: " . $e->getMessage());
+        } catch (Exception $e) {
+            throw new Exception("Erro genérico: " . $e->getMessage());
         }
     }
-}
 
-?>
+    public function searchData() {
+
+        $res = array();
+        $searchAll = $this->pdo->query("SELECT * FROM pessoa ORDER BY nome");
+        $res = $searchAll->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
+    }
+
+    public function emailExists($email) {
+        $query = $this->pdo->prepare("SELECT id FROM pessoa WHERE email = :email");
+        $query->bindValue(":email", $email);
+        $query->execute();
+        return $query->rowCount() > 0;
+    }
+
+
+    public function register($nome, $telefone, $email) {
+
+        if ($this->emailExists($email)) {
+            return false; //email existe
+        }
+
+        $insert = $this->pdo->prepare("INSERT INTO pessoa (nome, telefone, email) VALUES (:nome, :telefone, :email)");
+        $insert->bindValue(':nome', $nome);
+        $insert->bindValue(':telefone', $telefone);
+        $insert->bindValue(':email', $email);
+        $insert->execute();
+
+        return true; //registro realizado
+    }
+
+    public function delete($id) {
+        $query = $this->pdo->prepare("DELETE FROM pessoa WHERE id = :id");
+        $query->bindValue(':id', $id);
+        $query->execute();
+
+        return $query->rowCount() > 0; // Retorna true se a exclusão for bem-sucedida
+    }
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-require('pessoa.php');
+require_once 'Pessoa.php';
+$p = new Pessoa("crudpdo", "localhost", "root", "");
 ?>
 
 <!DOCTYPE html>
@@ -13,23 +14,39 @@ require('pessoa.php');
 </head>
 
 <body>
+
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-4">
-                <div class="rounded p-4" style="background-color: #f0f0f0;">
+                <div class="rounded p-4 bg-light">
                     <h1 class="mb-4">Cadastro</h1>
                     <form method="post">
+                        <?php
+                        if (isset($_POST['nome'])) {
+                            $nome = htmlspecialchars($_POST['nome']);
+                            $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_NUMBER_INT);
+                            $email = htmlspecialchars($_POST['email']);
+
+                            if (!empty($nome) && !empty($telefone) && !empty($email)) {
+                                if (!$p->register($nome, $telefone, $email)) {
+                                    echo '<div class="alert alert-secondary" role="alert">E-mail já cadastrado.</div>';
+                                }
+                            } else {
+                                echo "<div class='alert alert-danger' role='alert'>Preencha todos os campos!</div>";
+                            }
+                        }
+                        ?>
                         <div class="mb-3">
                             <label for="nome" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome">
+                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome" required>
                         </div>
                         <div class="mb-3">
                             <label for="telefone" class="form-label">Telefone</label>
-                            <input type="tel" class="form-control" id="telefone" name="telefone" size="15" placeholder="Digite seu telefone">
+                            <input type="tel" class="form-control" id="telefone" name="telefone" size="15" placeholder="Digite seu telefone" required>
                         </div>
                         <div class="mb-4">
                             <label for="email" class="form-label">E-mail</label>
-                            <input type="text" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
+                            <input type="text" class="form-control" id="email" name="email" placeholder="Digite seu e-mail" required>
                         </div>
                         <button type="submit" class="btn btn-primary w-100" name="Enviar">Enviar</button>
                     </form>
@@ -37,11 +54,10 @@ require('pessoa.php');
             </div>
             <div class="col-md-8">
                 <div class="container">
-                    <div style="max-height: 70vh; overflow-y: auto">
+                    <div class="rounded" style=" max-height: 70vh; overflow-y: auto">
                         <table class="table table-striped table-bordered table-hover">
                             <thead class="table-primary">
                                 <tr>
-                                    <th>ID</th>
                                     <th>Nome</th>
                                     <th>Telefone</th>
                                     <th>Email</th>
@@ -49,22 +65,30 @@ require('pessoa.php');
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php foreach ($rows as $row) : ?>
-                                    <tr>
-                                        <td><?php echo $row['id']; ?></td>
-                                        <td><?php echo $row['nome']; ?></td>
-                                        <td><?php echo $row['telefone']; ?></td>
-                                        <td><?php echo $row['email']; ?></td>
-                                        <td>
-                                            <form method="post">
-                                                <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                                                <button type="submit" class="btn btn-danger" name="Excluir">Excluir</button>
-                                                <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                                                <button type="submit" class="btn btn-warning" name="Editar">Editar</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
+                                <?php
+                                $dados = $p->searchData();
+
+                                if (count($dados) > 0) {
+                                    foreach ($dados as $dado) : ?>
+                                        <tr>
+                                            <td><?php echo $dado['nome']; ?></td>
+                                            <td><?php echo $dado['telefone']; ?></td>
+                                            <td><?php echo $dado['email']; ?></td>
+                                            <td>
+                                                <form method="post">
+                                                    <input type="hidden" name="id" value="<?php echo $dado['id']; ?>">
+                                                    <button type="submit" class="btn btn-danger" name="Excluir">Excluir</button>
+                                                    <input type="hidden" name="id" value="<?php echo $dado['id']; ?>">
+                                                    <button type="submit" class="btn btn-warning" name="Editar">Editar</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                <?php endforeach;
+                                } else {
+                                    echo '<div class="alert alert-warning" role="alert">Ainda não há pessoas cadastradas!</div>';
+                                }
+                                ?>
+
                             </tbody>
                         </table>
                     </div>
